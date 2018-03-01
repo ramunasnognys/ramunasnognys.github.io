9c4b913e7e677267030a859a24a030241b90f2a3
Ramunas <dev.nognys@gmail.com>
2018-03-01 10:50:55 +0200

    work section js no work

---
+++ assets/js/functions.js
@@ -0,0 +1,45 @@
+$(function() {
+	smoothScroll(300);
+	workBelt();
+
+	$("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
+	$(".biglink").fitText(1.5);
+
+	$('textarea').autosize();
+});
+
+// smoothScroll function is applied from the document ready function
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
+
+
+function workBelt() {
+
+  $(".trigger").remove();
+  $(".return").remove();
+
+  $('.thumb-container label').click(function() {
+    $('.work-belt').addClass("slided");
+    $('.work-container').show();
+  });
+
+  $('.work-return').click(function() {
+    $('.work-belt').removeClass("slided");
+    $('.work-container').hide(800);
+  });
+
+}
+
+
+
